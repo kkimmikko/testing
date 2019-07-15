a6de1d95664af13984ede2825a175982f8f32c0e
kkimmikko <52597107+kkimmikko@users.noreply.github.com>
2019-07-15 23:04:04 +0800

    Update parser.js

--- parser.js
+++ parser.js
@@ -9,17 +9,44 @@
 
 //var Lazy=require("lazy");
 //new Lazy(process.stdin);
+
 var fs = require('fs'),
     readline = require('readline');
 
 var rd = readline.createInterface({
-    input: fs.createReadStream('combatlog1.txt'),
+    input: fs.createReadStream('combatlog.txt'),
     output: process.stdout,
     console: false
 });
-
+var options = {
+  host: "localhost",
+  port: 80,
+  path: '/resource?id=foo&bar=baz',
+  method: 'POST'
+};
+var buffer = []
 rd.on('line', function(line) {
-    console.log(line);
-});
+   if (line[0] == '{') {
+     buffer = []
+     buffer.push(line)
+  } else if (line[0] == '}') {
+     // convert buffer to json
+     var type = parseInt(buffer[1].split(':')[1]) 
+     console.log('found type: ' + type)
+     if (type == 2 || type == 3) {
+             var inflictor = buffer[4].split(':')[1].trim()
+             if (inflictor == 'modifier_rune_arcane') {
+              // call vmix api
+              options.path = 'my new vmix path to close the overlay'
+              console.log("Whatever")
+              //http.request(options, function (res) {
+              //})
+              }
+             
+   }
+  } else {
+     buffer.push(line)
+  }
+   });
 
 //process.stdin.resume();
