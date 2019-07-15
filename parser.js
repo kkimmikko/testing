49c72544264ba783b1b78eae2fb4a2e2b610ded1
kkimmikko <52597107+kkimmikko@users.noreply.github.com>
2019-07-15 20:33:47 +0800

    Add files via upload

---
+++ parser.js
@@ -0,0 +1,25 @@
+/* var lineReader = require('readline').createInterface({
+    input: require('fs').createReadStream('combatlog1.txt')
+  });
+  
+  lineReader.on('line', function (line) {
+    if ('line' === "type: 4");
+      console.log('Line from file:', line);
+  }); */
+
+var Lazy=require("lazy");
+new Lazy(process.stdin);
+var fs = require('fs'),
+    readline = require('readline');
+
+var rd = readline.createInterface({
+    input: fs.createReadStream('combatlog1.txt'),
+    output: process.stdout,
+    console: false
+});
+
+rd.on('line', function(line) {
+    console.log(line);
+});
+
+process.stdin.resume();
