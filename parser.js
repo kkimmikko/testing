c7c045c5babc1091307af1e52fa91c7b5be5cab3
kkimmikko <52597107+kkimmikko@users.noreply.github.com>
2019-07-16 15:40:02 +0800

    test

--- parser.js
+++ parser.js
@@ -18,10 +18,12 @@ var rd = readline.createInterface({
     output: process.stdout,
     console: false
 });
+//http://127.0.0.1:8088/API/?Function=MultiViewOverlayOn&Input=smoke&Value=1
+var http = require("http");
 var options = {
-  host: "localhost",
-  port: 80,
-  path: '/resource?id=foo&bar=baz',
+  host: "127.0.0.1",
+  port: 8088,
+  path: '/API/?Function=MultiViewOverlayOn&Input=smoke&Value=1',
   method: 'POST'
 };
 var buffer = []
@@ -32,17 +34,17 @@ rd.on('line', function(line) {
   } else if (line[0] == '}') {
      // convert buffer to json
      var type = parseInt(buffer[1].split(':')[1]) 
-     console.log('found type: ' + type)
+     //console.log('found type: ' + type)
      if (type == 2 || type == 3) {
              var inflictor = buffer[4].split(':')[1].trim();
              var inflictor2 = buffer[5].split(':')[1].trim();
-             console.log(inflictor + ' - ' + inflictor2);
-             if (inflictor == 'modifier_rune_arcane' || inflictor2 == 'modifier_rune_arcane') {
+             //console.log(inflictor + ' - ' + inflictor2);
+             if (inflictor == 'modifier_rune_doubledamage' || inflictor2 == 'modifier_rune_doubledamage') {
               // call vmix api
               options.path = 'my new vmix path to close the overlay';
               console.log("whatever-----------------------------------------------------------------");
-              //http.request(options, function (res) {
-              //})
+              http.request(options, function (res) {
+              })
               }
              
    }
